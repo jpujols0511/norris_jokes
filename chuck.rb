be0f0bcb9e73379765d82c881423c0105ee626b8
Joan Pujols <joanpujols1998@gmail.com>
2019-06-07 17:04:49 -0400

    Fixed some text, ready for submission

--- chuck.rb
+++ chuck.rb
@@ -2,15 +2,15 @@ require "chuck_norris"
 
 class Chuckjokes
   def self.help
-    puts "Enter what you would like to do. For a random joke enter 'ruby app.rb random', for an id, enter 'ruby app.rb (enter # here)', to replace a name enter 'ruby app.rb (fist name, last name)'"
+    puts "Hello to the worst joke teller of Ruby. For a random joke enter 'ruby app.rb random', for an id, enter 'ruby app.rb (enter # here)', to replace a name enter 'ruby app.rb (fist name, last name)'"
   end
   def self.get_joke_by_id(id)
     id_joke = ChuckNorris::JokeFinder.find_joke(id)
     puts id_joke.joke
     puts "ID #{id_joke.id}"
   end
-  def self.replace_name(nam1, nam2)
-      change = ChuckNorris::JokeFinder.get_joke(first_name: nam1, last_name: nam2 )
+  def self.replace_name(userName1, userName2)
+      change = ChuckNorris::JokeFinder.get_joke(first_name: userName1, last_name: userName2 )
       puts change.joke
       puts "ID #{change.id}"
   end
